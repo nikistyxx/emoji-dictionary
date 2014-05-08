8b7a654bbe1279326769d2de632d5745b561a330
Niki Selken <nikiselken@gmail.com>
2014-05-08 10:36:04 -0400

    more done

--- header.php
+++ header.php
@@ -1,19 +1,19 @@
-        <header>
-            <div id="boring">
-                <div id="wtf">
-                    <a href="index.php"><img src="img/wtf.jpg"></a>
-                </div>
-                    <h2>The Emoji Dictionary</h2>
-                <h3>Discover Emoji Definitions and Add Your Own!</h3>
-            </div>
-            <div id="search">
-                <label for="EmojiCode">Search for Emoji</label><br><br>
-                <div id="searchs">
-                    <input type="text" name="EmojiCode"/>
-                </div>
-                <div class="Add">
-                   <button type="submit">Submit</button>
-                </div>
-               
-            </div>
-        </header>
+<header>
+    <div id="boring">
+        <div id="wtf">
+            <a href="index.php"><img alt="logo" src="img/wtf.jpg"></a>
+        </div>
+            <h2>The Emoji Dictionary</h2>
+            <h3>Discover Emoji Definitions and Add Your Own!</h3>
+    </div>
+    <div id="search">
+        <label for="EmojiCode">Search for Emoji</label><br><br>
+        <div id="searchs">
+            <input type="text" name="EmojiCode"/>
+        </div>
+        <div class="Add">
+           <button type="submit">Submit</button>
+        </div>
+
+    </div>
+</header>
--- index.php
+++ index.php
@@ -37,7 +37,6 @@ if( isset( $_GET['action'] ) )
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
   </head>
   <body>
-    </div>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
